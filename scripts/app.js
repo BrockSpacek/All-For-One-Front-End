14bd204169ca8f5704b9584deac16b7f8e594782
BrockSpacek <brockspacek1@gmail.com>
2025-01-23 13:20:35 -0800

    Lots of front end additions

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+const playBtn = document.getElementById("playBtn");
+const aboutBtn = document.getElementById("aboutBtn");
+const mainMenuBtn = document.getElementById("mainMenuBtn");
+
+
+aboutBtn.addEventListener('click', () => {
+    window.location.href = '../pages/About.html';
+});
+
+mainMenuBtn.addEventListener('click', () => {
+    window.location.href = '../index.html';
+});
+
+
+
+
+
+
