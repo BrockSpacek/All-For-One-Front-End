28b2df5929608325dd12cb97d0b3402db58a4615
BrockSpacek <brockspacek1@gmail.com>
2025-01-23 13:33:58 -0800

    buttons and css

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,16 @@
 const playBtn = document.getElementById("playBtn");
 const aboutBtn = document.getElementById("aboutBtn");
 const mainMenuBtn = document.getElementById("mainMenuBtn");
+const gameOneBtn = document.getElementById("gameOneBtn");
+const gameTwoBtn = document.getElementById("gameTwoBtn");
+const gameThreeBtn = document.getElementById("gameThreeBtn");
+const gameFourBtn = document.getElementById("gameFourBtn");
+const gameFiveBtn = document.getElementById("gameFiveBtn");
+const gameSixBtn = document.getElementById("gameSixBtn");
+const gameSevenBtn = document.getElementById("gameSevenBtn");
+const gameEightBtn = document.getElementById("gameEightBtn");
+const gameNineBtn = document.getElementById("gameNineBtn");
+const gameTenBtn = document.getElementById("gameTenBtn");
 
 
 aboutBtn.addEventListener('click', () => {
@@ -11,7 +21,45 @@ mainMenuBtn.addEventListener('click', () => {
     window.location.href = '../index.html';
 });
 
+gameOneBtn.addEventListener('click', () => {
+    window.location.href = '../pages/GameOne.html'
+});
 
+gameTwoBtn.addEventListener('click', () => {
+    window.location.href = '../pages/GameTwo.html'
+});
+
+gameThreeBtn.addEventListener('click', () => {
+    window.location.href = '../pages/GameThree.html'
+});
+
+gameFourBtn.addEventListener('click', () => {
+    window.location.href = '../pages/GameFour.html'
+});
+
+gameFiveBtn.addEventListener('click', () => {
+    window.location.href = '../pages/GameFive.html'
+});
+
+gameSixBtn.addEventListener('click', () => {
+    window.location.href = '../pages/GameSix.html'
+});
+
+gameSevenBtn.addEventListener('click', () => {
+    window.location.href = '../pages/GameSeven.html'
+});
+
+gameEightBtn.addEventListener('click', () => {
+    window.location.href = '../pages/GameEight.html'
+});
+
+gameNineBtn.addEventListener('click', () => {
+    window.location.href = '../pages/GameNine.html'
+});
+
+gameTenBtn.addEventListener('click', () => {
+    window.location.href = '../pages/GameTen.html'
+});
 
 
 
