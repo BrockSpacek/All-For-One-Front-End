4c9b964129ae329ed56e52fe802973659e8e23f7
BrockSpacek <brockspacek1@gmail.com>
2025-01-24 11:30:17 -0800

    Logic and styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,66 +1,17 @@
-const playBtn = document.getElementById("playBtn");
-const aboutBtn = document.getElementById("aboutBtn");
-const mainMenuBtn = document.getElementById("mainMenuBtn");
-const gameOneBtn = document.getElementById("gameOneBtn");
-const gameTwoBtn = document.getElementById("gameTwoBtn");
-const gameThreeBtn = document.getElementById("gameThreeBtn");
-const gameFourBtn = document.getElementById("gameFourBtn");
-const gameFiveBtn = document.getElementById("gameFiveBtn");
-const gameSixBtn = document.getElementById("gameSixBtn");
-const gameSevenBtn = document.getElementById("gameSevenBtn");
-const gameEightBtn = document.getElementById("gameEightBtn");
-const gameNineBtn = document.getElementById("gameNineBtn");
-const gameTenBtn = document.getElementById("gameTenBtn");
+const firstName = document.getElementById("firstName");
+const dekuText = document.getElementById("dekuText");
 
 
-aboutBtn.addEventListener('click', () => {
-    window.location.href = '../pages/About.html';
+const sayHelloApi = async (names) => {
+    const promise = await fetch(`https://spacekballforoneapi-ecevf6h7b0c2bfhq.westus-01.azurewebsites.net/SayHello/Greeting/${names}`)
+    const data = await promise.text();
+    return data
+}
+
+sayHelloApi();
+
+firstName.addEventListener('keydown', async (event) => {
+    if (event.key === 'Enter') {
+        dekuText.textContent = await sayHelloApi(firstName.value);
+    }
 });
-
-mainMenuBtn.addEventListener('click', () => {
-    window.location.href = '../index.html';
-});
-
-gameOneBtn.addEventListener('click', () => {
-    window.location.href = '../pages/GameOne.html'
-});
-
-gameTwoBtn.addEventListener('click', () => {
-    window.location.href = '../pages/GameTwo.html'
-});
-
-gameThreeBtn.addEventListener('click', () => {
-    window.location.href = '../pages/GameThree.html'
-});
-
-gameFourBtn.addEventListener('click', () => {
-    window.location.href = '../pages/GameFour.html'
-});
-
-gameFiveBtn.addEventListener('click', () => {
-    window.location.href = '../pages/GameFive.html'
-});
-
-gameSixBtn.addEventListener('click', () => {
-    window.location.href = '../pages/GameSix.html'
-});
-
-gameSevenBtn.addEventListener('click', () => {
-    window.location.href = '../pages/GameSeven.html'
-});
-
-gameEightBtn.addEventListener('click', () => {
-    window.location.href = '../pages/GameEight.html'
-});
-
-gameNineBtn.addEventListener('click', () => {
-    window.location.href = '../pages/GameNine.html'
-});
-
-gameTenBtn.addEventListener('click', () => {
-    window.location.href = '../pages/GameTen.html'
-});
-
-
-
-
